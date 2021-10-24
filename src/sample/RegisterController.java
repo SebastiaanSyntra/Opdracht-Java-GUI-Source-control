c56abf351f2b32141ab550cb1276a9bfcf84bf30
SebastiaanSyntra <84094489+SebastiaanSyntra@users.noreply.github.com>
2021-10-24 15:37:24 +0200

    Eerste versie: scherm zonder functionaliteit

---
+++ src/sample/RegisterController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
+import java.util.Optional;
+
+public class RegisterController {
+
+    @FXML
+    private TextField Txtname;
+    @FXML
+    private TextField Txtmail;
+    @FXML
+    private TextField Txtpassword;
+    @FXML
+    private Button Btnregister;
+
+
+}
